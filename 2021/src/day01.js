42b0fdfe27e63d7a72746c74a6b3395d94115bb1
msmol <msmol@users.noreply.github.com>
2021-12-01 16:58:23 -0500

    Remove lodash from solution

--- 2021/src/day01.js
+++ 2021/src/day01.js
@@ -1,23 +1,21 @@
 const fs = require('fs');
-const _ = require('lodash');
 
-const data = _.chain(fs.readFileSync('input/day01.input', 'utf8'))
+const data = fs.readFileSync('input/day01.input', 'utf8')
   .split('\n')
   .map(i => parseInt(i, 10))
-  .reject(_.isNaN)
-  .value();
+  .filter(x => !isNaN(x));
 
-const increases = _.filter(data, (d, i) => d > data[i-1]).length;
+const increases = data.filter((d, i) => d > data[i-1]).length;
 
 console.log(increases);
 
-const windows = _.reduce(data, (result, value, key) => {
+const windows = data.reduce((result, value, key) => {
   const window = data.slice(key, key+3);
   if (window.length === 3) result.push(window);
   return result;
 }, []);
 
-const sums = windows.map(_.sum);
-const windowIncreases = _.filter(sums, (sum, i) => sum > sums[i-1]).length;
+const sums = windows.map(window => window.reduce((acc, curr) => acc + curr, 0));
+const windowIncreases = sums.filter((sum, i) => sum > sums[i-1]).length;
 
 console.log(windowIncreases);

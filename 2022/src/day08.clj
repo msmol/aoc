c085ed66c288a2d1a356bca230e07172729848e8
msmol <msmol@users.noreply.github.com>
2022-12-08 17:12:23 -0500

    Day 08 2022

---
+++ 2022/src/day08.clj
@@ -0,0 +1,70 @@
+(ns day08
+  (:require [clojure.string :as str]))
+
+(defn transpose [m]
+  (apply mapv vector m))
+
+(def trees (->> "./2022/input/day08.input"
+                (slurp)
+                (#(str/split %1 #"\n"))
+                (map #(str/split %1 #""))
+                (map (fn [row] (map #(Integer/parseInt %1) row)))
+                (transpose)))
+
+(defn is-visible?
+  "Determines whether a tree at a given location is visible"
+  [all-trees x y]
+  (let [transposed (transpose trees)
+        left (subvec (get transposed y) 0 x)
+        right (subvec (get transposed y) (+ x 1) (count (get all-trees y)))
+        above (subvec (get all-trees x) 0 y)
+        below (subvec (get all-trees x) (+ y 1) (count (get all-trees x)))
+        tree (get-in trees [x y])]
+    (->> [left right above below]
+         (some (fn [direction]
+                 (every? (fn [t]
+                           (> tree t)) direction))))))
+
+(println "Part 1:" (->> trees
+                        (map-indexed (fn [y column]
+                                       (map-indexed (fn [x _]
+                                                      (is-visible? trees x y)) column)))
+                        flatten
+                        (filter #(true? %1))
+                        count))
+
+(defn find-first
+  "Find the first item in a list that satisfies f"
+  [f coll]
+  (first (filter f coll)))
+
+(defn get-view
+  "Gets how far past it's neighbours a tree can 'see'"
+  [height neighbours]
+  (let [first-blocker (find-first #(>= %1 height) neighbours)]
+    (cond
+      (nil? first-blocker) (count neighbours)
+      :else (inc (.indexOf neighbours first-blocker)))
+    ))
+
+(defn scenic-score
+  "Gets the scenic score of a tree"
+  [all-trees x y]
+  (let [transposed (transpose trees)
+        left (reverse (subvec (get transposed y) 0 x))
+        right (subvec (get transposed y) (+ x 1) (count (get all-trees y)))
+        above (reverse (subvec (get all-trees x) 0 y))
+        below (subvec (get all-trees x) (+ y 1) (count (get all-trees x)))
+        tree (get-in trees [x y])
+        left-view (get-view tree left)
+        right-view (get-view tree right)
+        above-view (get-view tree above)
+        below-view (get-view tree below)]
+    (* left-view right-view above-view below-view)))
+
+(println "Part 2:" (->> trees
+                        (map-indexed (fn [y column]
+                                       (map-indexed (fn [x _]
+                                                      (scenic-score trees x y)) column)))
+                        flatten
+                        (apply max)))
